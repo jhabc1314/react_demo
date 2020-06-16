814a90ac83de344bd534fcc7b8cab0c7478aa4d0
江恒 <jiangheng@chelun.com>
2020-06-16 09:21:50 +0800

    master

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let my = {firstName:'jh', secondName:'dou'};
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {format(my)}
         </p>
         <a
           className="App-link"
@@ -16,11 +19,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
       </header>
     </div>
   );
+}
+
+function format({ firstName, secondName }) {
+  return `${firstName} ${secondName}`;
 }
 
 export default App;
